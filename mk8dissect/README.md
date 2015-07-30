047954e39b08727831fc3812d9fe2d6d3f3082ed
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2015-07-30 16:14:22 -0700

    parse all records in packet

--- mk8dissect/README.md
+++ mk8dissect/README.md
@@ -1,1 +1,5 @@
-Protocol dissector based on description from http://mk8.tockdom.com/wiki/MK8_Network_Protocol
+Wireshark Protocol dissector based on description from http://mk8.tockdom.com/wiki/MK8_Network_Protocol
+
+Currently just parses the packet and record headers; no game specific data is parsed
+
+Might be useful for investigating Mario Kart 8 and Splatoon. Or not.
--- mk8dissect/mk8base.lua
+++ mk8dissect/mk8base.lua
@@ -4,6 +4,7 @@ local pf_magic = ProtoField.new("Magic", "mk8base.magic", ftypes.UINT32)
 local pf_unknown0 = ProtoField.new("Unknown 0", "mk8base.unknown0", ftypes.UINT32)
 local pf_timer = ProtoField.new("Timer", "mk8base.timer", ftypes.UINT16)
 local pf_remote_timer = ProtoField.new("Remote Timer", "mk8base.remote_timer", ftypes.UINT16)
+local pf_record = ProtoField.new("Record", "mk8base.record", ftypes.BYTES)
 local pf_client_slot = ProtoField.new("Client slot", "mk8base.client_slot", ftypes.UINT16)
 local pf_length = ProtoField.new("Length", "mk8base.length", ftypes.UINT16)
 local pf_receiver_slots = ProtoField.new("Receiver slots", "mk8base.receiver_slots", ftypes.UINT32, nil, base.HEX)
@@ -14,12 +15,17 @@ local pf_data = ProtoField.new("Data", "mk8base.data", ftypes.BYTES)
 
 local pf_checksum = ProtoField.new("Checksum", "mk8base.checksum", ftypes.BYTES)
 
+local pf_numrecords = ProtoField.new("Number of records", "mk8base.num_records", ftypes.UINT32)
+
+
 mk8base_proto.fields = {
 	pf_magic, pf_unknown0, pf_timer, pf_remote_timer,
+	pf_record,
 	pf_client_slot, pf_length, pf_receiver_slots, pf_sender_pid,
 	pf_unknown1, pf_unknown2,
 	pf_data,
-	pf_checksum
+	pf_checksum,
+	pf_numrecords
 }
 
 -- dofile("C:\\Users\\zhuowei\\Documents\\winprogress\\mk8dissect\\mk8base.lua")
@@ -38,15 +44,30 @@ function mk8base_proto.dissector(tvbuf,pktinfo,root)
 	tree:add(pf_unknown0, tvbuf:range(4, 4))
 	tree:add(pf_timer, tvbuf:range(8, 2))
 	tree:add(pf_remote_timer, tvbuf:range(10, 2))
-	tree:add(pf_client_slot, tvbuf:range(12, 2))
-	tree:add(pf_length, tvbuf:range(14, 2))
-	tree:add(pf_receiver_slots, tvbuf:range(16, 4))
-	tree:add(pf_sender_pid, tvbuf:range(20, 4))
-	tree:add(pf_unknown1, tvbuf:range(24, 4))
-	tree:add(pf_unknown2, tvbuf:range(28, 4))
-	tree:add(pf_data, tvbuf:range(32, tvbuf:range(14, 2):uint()))
+	local recordbegin = 12
+	local numrecords = 0
+	while recordbegin < (pktlen - 16) do
+		numrecords = numrecords + 1
+ 		local datalen = tvbuf:range(recordbegin + 2, 2):uint()
 
+		local recordlen = 20+datalen
+		local recordmod = recordlen % 4
+		if recordmod ~= 0 then
+			recordlen = recordlen + (4-recordmod)
+		end
+		local stree = tree:add(pf_record, tvbuf:range(recordbegin, recordlen))
+		stree:add(pf_client_slot, tvbuf:range(recordbegin + 0, 2))
+		stree:add(pf_length, tvbuf:range(recordbegin + 2, 2))
+		stree:add(pf_receiver_slots, tvbuf:range(recordbegin + 4, 4))
+		stree:add(pf_sender_pid, tvbuf:range(recordbegin + 8, 4))
+		stree:add(pf_unknown1, tvbuf:range(recordbegin + 12, 4))
+		stree:add(pf_unknown2, tvbuf:range(recordbegin + 16, 4))
+		stree:add(pf_data, tvbuf:range(recordbegin + 20, datalen))
+
+		recordbegin = recordbegin + recordlen
+	end
 	tree:add(pf_checksum, tvbuf:range(pktlen - 16, 16))
+	tree:add(pf_numrecords, numrecords)
 	return pktlen
 end
 
